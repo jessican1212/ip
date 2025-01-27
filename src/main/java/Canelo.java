3243a435b205314c9d35e1af80adce6f69175429
jessicang <jessicang1212@gmail.com>
2025-01-27 15:34:15 +0800

    Change name of chatbot

--- src/main/java/Canelo.java
+++ src/main/java/Canelo.java
@@ -1,14 +1,8 @@
 public class Canelo {
     public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-//        System.out.println("Hello from\n" + logo);
         String intro = """
                 ____________________________________________________________
-                 Hello! I'm Canelo!!!
+                 Hello! I'm Canelo :)
                  What can I do for you?
                 ____________________________________________________________
                  Bye. Hope to see you again soon!
