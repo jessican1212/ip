218442dea0a3ec39b5199536640c8ef0d728dce0
jessicang <jessicang1212@gmail.com>
2025-01-27 15:52:50 +0800

    Add list functionality

--- src/main/java/Canelo.java
+++ src/main/java/Canelo.java
@@ -7,12 +7,23 @@ public class Canelo {
                  Hello! I'm Canelo :)
                  What can I do for you?
                  ____________________________________________________________""");
+        String[] list = new String[100];
+        int numInList = 0;
         String keyboardInput;
         Scanner in = new Scanner(System.in);
         keyboardInput = in.nextLine();
         while (!keyboardInput.equals("bye")) {
             System.out.println("____________________________________________________________");
-            System.out.println(keyboardInput);
+            if (keyboardInput.equals("list")) {
+                for (int i = 0; i < numInList; i++) {
+                    int iPlusOne = i + 1;
+                    System.out.println(iPlusOne + ": " + list[i]);
+                }
+            } else {
+                list[numInList] = keyboardInput;
+                numInList++;
+                System.out.println("added: " + keyboardInput);
+            }
             System.out.println("____________________________________________________________");
             keyboardInput = in.nextLine();
         }
