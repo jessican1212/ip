233fda4cce6b86a55f00f5436c8f60d9b9a59790
jessicang <jessicang1212@gmail.com>
2025-01-27 15:47:34 +0800

    Add echo functionality

--- src/main/java/Canelo.java
+++ src/main/java/Canelo.java
@@ -7,6 +7,15 @@ public class Canelo {
                  Hello! I'm Canelo :)
                  What can I do for you?
                  ____________________________________________________________""");
+        String keyboardInput;
+        Scanner in = new Scanner(System.in);
+        keyboardInput = in.nextLine();
+        while (!keyboardInput.equals("bye")) {
+            System.out.println("____________________________________________________________");
+            System.out.println(keyboardInput);
+            System.out.println("____________________________________________________________");
+            keyboardInput = in.nextLine();
+        }
         System.out.println("""
                 ____________________________________________________________
                  Bye. Hope to see you again soon!
