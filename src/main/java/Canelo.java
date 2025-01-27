d52a342705970f93c495957b530e6ff61796d5a9
jessicang <jessicang1212@gmail.com>
2025-01-27 16:30:36 +0800

    Add mark and unmark functionality

--- src/main/java/Canelo.java
+++ src/main/java/Canelo.java
@@ -7,25 +7,37 @@ public class Canelo {
                  Hello! I'm Canelo :)
                  What can I do for you?
                  ____________________________________________________________""");
-        String[] list = new String[100];
+        Task[] list = new Task[100];
         int numInList = 0;
-        String keyboardInput;
+        String line;
         Scanner in = new Scanner(System.in);
-        keyboardInput = in.nextLine();
-        while (!keyboardInput.equals("bye")) {
+        line = in.nextLine();
+        while (!line.equals("bye")) {
             System.out.println("____________________________________________________________");
-            if (keyboardInput.equals("list")) {
+            String[] keyboardInput = line.split(" ");
+            if (keyboardInput.length == 1 && keyboardInput[0].equals("list")) {
                 for (int i = 0; i < numInList; i++) {
                     int iPlusOne = i + 1;
-                    System.out.println(iPlusOne + ": " + list[i]);
+                    System.out.println(iPlusOne + ":[" + list[i].getStatusIcon() + "] " + list[i].getDescription());
                 }
-            } else {
-                list[numInList] = keyboardInput;
+            } else if (keyboardInput.length == 2 && keyboardInput[0].equals("mark")) {
+                Task task = list[Integer.parseInt(keyboardInput[1]) - 1];
+                task.markDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("[X] " + task.getDescription());
+            } else if (keyboardInput.length == 2 && keyboardInput[0].equals("unmark")) {
+                Task task = list[Integer.parseInt(keyboardInput[1]) - 1];
+                task.markNotDone();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("[ ] " + task.getDescription());
+            } else if (keyboardInput.length > 0) {
+                Task task = new Task(line);
+                list[numInList] = task;
                 numInList++;
-                System.out.println("added: " + keyboardInput);
+                System.out.println("added: " + line);
             }
             System.out.println("____________________________________________________________");
-            keyboardInput = in.nextLine();
+            line = in.nextLine();
         }
         System.out.println("""
                 ____________________________________________________________
