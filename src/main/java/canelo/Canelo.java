aa7a4034b6fbc6c5dbb809c83c3d063659c936b5
jessicang <jessicang1212@gmail.com>
2025-02-15 16:41:32 +0800

    Add delete functionality, change task list to be of type ArrayList

--- src/main/java/canelo/Canelo.java
+++ src/main/java/canelo/Canelo.java
@@ -1,11 +1,12 @@
 package canelo;
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Canelo {
     static Scanner input = new Scanner(System.in);
     static int numTasks = 0;
-    static Task[] list = new Task[100];
+    static ArrayList<Task> tasks = new ArrayList<>();
 
     static final String BY = "/by";
     static final String FROM = "/from";
@@ -18,6 +19,7 @@ public class Canelo {
     static final int DEADLINE_BY_LENGTH = 4;
     static final int MINIMUM_EVENT_LENGTH = 6;
     static final int EVENT_TO_LENGTH = 4;
+    static final int MINIMUM_DELETE_LENGTH = 7;
 
     static final String LINE = "____________________________________________________________";
 
@@ -48,6 +50,7 @@ public class Canelo {
             case "todo" -> "todo";
             case "deadline" -> "deadline";
             case "event" -> "event";
+            case "delete" -> "delete";
             default -> "unknown";
         };
     }
@@ -55,9 +58,9 @@ public class Canelo {
     private static void handleList() {
         for (int i = 0; i < numTasks; i++) {
             int taskNumber = i + 1;
-            String typeIcon = list[i].getTypeIcon();
-            String statusIcon = list[i].getStatusIcon();
-            String description = list[i].getDescription();
+            String typeIcon = tasks.get(i).getTypeIcon();
+            String statusIcon = tasks.get(i).getStatusIcon();
+            String description = tasks.get(i).getDescription();
             System.out.println(taskNumber + ".[" + typeIcon + "][" + statusIcon + "] " + description);
         }
     }
@@ -70,9 +73,9 @@ public class Canelo {
         if (isInvalidTaskNumber(taskNumber)) {
             throw new CaneloException("Please input a valid task number between 1 and " + numTasks + ".");
         }
-        Task task = list[taskNumber - 1];
+        Task task = tasks.get(taskNumber - 1);
         task.markDone();
-        System.out.println("Nice! I've marked this task as done:\n[X] " + task.getDescription());
+        System.out.println("Nice! I've marked this task as done:\n[" + task.getTypeIcon() + "][X] " + task.getDescription());
     }
 
     private static void handleUnmark(String userInput) throws CaneloException {
@@ -83,15 +86,15 @@ public class Canelo {
         if (isInvalidTaskNumber(taskNumber)) {
             throw new CaneloException("Please input a valid task number.");
         }
-        Task task = list[taskNumber - 1];
+        Task task = tasks.get(taskNumber - 1);
         task.markNotDone();
-        System.out.println("OK, I've marked this task as not done yet:\n[ ] " + task.getDescription());
+        System.out.println("OK, I've marked this task as not done yet:\n[" + task.getTypeIcon() + "][ ] " + task.getDescription());
     }
 
     private static void handleDeadline(String userInput) throws CaneloException {
         int indexOfBy = userInput.indexOf(BY);
         if (indexOfBy <= MINIMUM_DEADLINE_LENGTH) {
-            throw new CaneloException("Submit a canelo.Deadline using `taskName /by taskBy` format.");
+            throw new CaneloException("Submit a Deadline using `taskName /by taskBy` format.");
         }
         String deadlineName = userInput.substring(MINIMUM_DEADLINE_LENGTH, indexOfBy - 1);
         String deadlineBy = userInput.substring(indexOfBy + DEADLINE_BY_LENGTH);
@@ -104,7 +107,7 @@ public class Canelo {
         int indexOfFrom = userInput.indexOf(FROM);
         int indexOfTo = userInput.indexOf(TO);
         if (indexOfFrom <= MINIMUM_EVENT_LENGTH || indexOfTo <= MINIMUM_EVENT_LENGTH) {
-            throw new CaneloException("Submit an canelo.Event using /from and /to format.");
+            throw new CaneloException("Submit an Event using /from and /to format.");
         }
         String eventName = userInput.substring(MINIMUM_EVENT_LENGTH, indexOfFrom - 1);
         String eventFrom = userInput.substring(indexOfFrom + MINIMUM_EVENT_LENGTH, indexOfTo - 1);
@@ -123,6 +126,22 @@ public class Canelo {
         printTaskAdded(task);
     }
 
+    private static void handleDelete(String userInput) throws CaneloException {
+        if (userInput.length() <= MINIMUM_DELETE_LENGTH) {
+            throw new CaneloException("Please add a task number to delete.");
+        }
+        int taskNumber = Integer.parseInt(userInput.substring(MINIMUM_DELETE_LENGTH));
+        if (isInvalidTaskNumber(taskNumber)) {
+            throw new CaneloException("Please input a valid task number between 1 and " + numTasks + ".");
+        }
+        Task taskToDelete = tasks.get(taskNumber - 1);
+        System.out.println("Noted. I've removed this task:");
+        System.out.println("    [" + taskToDelete.getTypeIcon() + "]["+taskToDelete.getStatusIcon() + "] "+taskToDelete.getDescription());
+        tasks.remove(taskToDelete);
+        numTasks -= 1;
+        System.out.println("Now you have " + numTasks + " tasks in the list.");
+    }
+
     private static void printTaskAdded(Task task) {
         System.out.println("Got it. I've added this task:");
         System.out.println("    [" + task.getTypeIcon() + "][" + task.getStatusIcon()+"] " + task.getDescription());
@@ -130,7 +149,7 @@ public class Canelo {
     }
 
     private static void addTask(Task task) {
-        list[numTasks] = task;
+        tasks.add(task);
         numTasks += 1;
     }
 
@@ -164,6 +183,9 @@ public class Canelo {
                     break;
                 case "event":
                     handleEvent(userInput);
+                    break;
+                case "delete":
+                    handleDelete(userInput);
                     break;
                 default:
                     throw new CaneloException("Unknown task type.");
